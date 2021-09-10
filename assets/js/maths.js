14313a40ec97c6d56530535b16a9efab77767ea6
John Traas <j.j.h.traas@gmail.com>
2021-09-10 04:08:00 +0200

    add submit button hover styles

---
+++ assets/js/maths.js
@@ -0,0 +1,46 @@
+console.log("Wololo")
+
+const submitButton = document.querySelector('.submit-answer');
+const submitText = document.querySelector('.submit-text');
+const checkIcon = document.querySelector('.fa-check-circle');
+
+submitButton.addEventListener('mouseover', () => {
+    let appliedStyle = `
+                transition: all .28s ease-in-out;
+                transform: translateX(3.2em)
+                `;
+    let buttonWidth = `
+                border-radius: .8em;
+                width: 8em;
+                `;
+    submitButton.style = buttonWidth;
+    checkIcon.style = appliedStyle;
+
+    for (opacity = 0; opacity < 1.1; opacity = opacity + 0.1) 
+    {           
+        setTimeout(() => {
+            submitText.style.opacity = opacity;
+        },300)                       
+    } 
+});
+submitButton.addEventListener('mouseleave', () => {
+    
+    // submitText.innerHTML = "";
+    let appliedStyle = `
+                transition: all .28s ease-in-out;
+                transform: translateX(0)
+                `;
+    let buttonWidth = `
+                border-radius: 50%;
+                width: 75px;
+                `;
+    submitButton.style = buttonWidth;
+    checkIcon.style = appliedStyle;
+    for (opacity = 1; opacity >= 0; opacity = opacity - 1) 
+    {   
+        submitText.style.opacity = opacity;        
+        // setTimeout(() => {
+        //     submitText.style.opacity = opacity;
+        // },10)                       
+    }
+});
